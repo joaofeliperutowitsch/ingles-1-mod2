8b06a2e07a633c93b8d144063add14aef4c736d8
joaofeliperutowitsch <jfrutowitsch@gmail.com>
2022-08-03 21:23:27 -0300

    1.Create 3 variables, each with an array:

---
+++ script.js
@@ -0,0 +1,74 @@
+let comedy = [
+    {
+        title: "Projeto X - Uma Festa Fora de Controle",
+        director: "Nima Nourizadeh",
+        year: 2012
+    },
+
+    {
+        title: "ted-2",
+        director: "Seth MacFarlane",
+        year: 2015
+    },
+
+    {
+        title: "Se Beber, Não Case! 3",
+        director: "Todd Phillips",
+        year: 2013
+    }
+]
+
+console.log(comedy)
+
+let action = [
+    {
+        title: "Piratas do Caribe",
+        director: "Rob Marshall",
+        year: 2011  
+    },
+
+    {
+        title: "Rocky II - A revanche",
+        director: "Sylvester Stallone",
+        year: 1979
+    },
+
+    {
+        title: "Velozes e Furiosos 5: Operação Rio",
+        director: "Justin Lin",
+        year: 2011
+    }
+]
+
+console.log(action)
+
+let fantasy = [
+    {
+        title: "Harry Potter",
+        director: "Chris Columbus",
+        year: 2001
+    },
+
+    {
+        title: "Vingadores: Ultimato",
+        director: "Anthony Russo, Joe Russo",
+        year: 2019
+    },
+
+    {
+        title: "Jogos Vorazes",
+        director: "Gary Ross",
+        year: 2012
+    }
+]
+
+console.log(fantasy)
+
+let movie = fantasy[0].title
+if (movie === "Rocky Balboa"){
+    console.log('MY FAVORIT MOVIE IS:' , filme)
+}else {
+    console.log('NOT EVEN 5 STARS!!10!')
+}
+
+
